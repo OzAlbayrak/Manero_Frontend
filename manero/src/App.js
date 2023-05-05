45129796790e6c8db336dd421bb47faf9c819031
jesperforsen2022 <lester_orkester@hotmail.com>
2023-05-05 17:16:48 +0200

    added router dom and HomeView

--- manero/src/App.js
+++ manero/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeView from './Views/HomeView';
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Routes>
+          <Route path='/' element = {<HomeView/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
