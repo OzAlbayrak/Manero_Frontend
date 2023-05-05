00f314c5e3d95794ecf2e54ca629cec06b958fe8
Per Norin <pelle.norin.79@gmail.com>
2023-05-05 15:26:19 +0200

    Remade header

---
+++ manero/src/components/individuals/SideIcon.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const SideIcon = ({ isMenu }) => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	const openMenu = () => {
+		setToggleMenu(!toggleMenu);
+		console.log('toggle menu');
+	};
+
+	function goBack() {
+		window.history.back();
+		console.log('click');
+	}
+
+	return (
+		<div>
+			{isMenu ? (
+				<i className='fa-sharp fa-light fa-bars' onClick={openMenu}></i>
+			) : (
+				<i className='fa-regular fa-chevron-left' onClick={goBack}></i>
+			)}
+		</div>
+	);
+};
+
+export default SideIcon;
--- manero/src/components/sections/Header.js
+++ manero/src/components/sections/Header.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Logo from '../individuals/Logo'
-import Menu from '../individuals/Menu'
-import Cart from '../individuals/Cart'
+import React from 'react';
+import Logo from '../individuals/Logo';
+import Menu from '../individuals/Menu';
+import Cart from '../individuals/Cart';
+import SideIcon from '../individuals/SideIcon';
 
-const Header = () => {
-  return (
-    <section className="header container">
-        <Menu/>
-        <Logo/>
-        <Cart/>
-    </section>
-  )
-}
+const Header = ({ title, hasSideIcon, isMenu, hasCart }) => {
+	return (
+		<header className='headersection container'>
+			{/* <Menu/> */}
+			{hasSideIcon && <SideIcon isMenu={isMenu} />}
+			<Logo title={title} />
+			{hasCart && <Cart />}
+		</header>
+	);
+};
 
-export default Header
+export default Header;
