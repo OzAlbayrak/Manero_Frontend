5641be718758b2b38ec0bc50e16d5e16a46198d3
jenwiss <jenny.wisselqvist94@gmail.com>
2023-05-08 16:21:03 +0200

    Sidebar + showcase uppdate

---
+++ manero/src/components/individuals/SideMenu.js
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+
+const SideMenu = () => {
+    
+    const [show, setShow] = useState(false);
+  
+      return (
+          <main className={show ? 'space-toggle' : null}>
+          <header className={`header ${show ? 'space-toggle' : null}`}>
+            <div className='header-toggle' onClick={() => setShow(!show)}>
+              <i className={`fa-light fa-bars-staggered ${show ? 'fa-light fa-xmark' : null}`}></i>
+            </div>
+          </header>
+          <aside className={`sidebar ${show ? 'show' : null}`}>
+        
+            <nav className='nav'>
+              <div>
+                <NavLink to="/HomeView" className='nav-logo'><span className='nav-logo-name'>Homepage</span></NavLink>
+  
+                <div className='nav-list'>
+                  <NavLink to="/" className='nav-link active'><span className='nav-link-name'>Products</span></NavLink>
+                  <NavLink to="/" className='nav-link'><span className='nav-link-name'>About us</span></NavLink>
+                  <NavLink to="/" className='nav-link'><span className='nav-link-name'>Contact</span></NavLink>
+                </div>
+              </div>
+    
+              <NavLink to="/" className='nav-link'><span className='nav-link-name'>Log in</span></NavLink>
+            </nav>
+          </aside>
+        </main>
+      )
+  }
+  export default SideMenu
--- manero/src/components/sections/Showcase.js
+++ manero/src/components/sections/Showcase.js
@@ -3,18 +3,95 @@ import React from 'react'
 const Showcase = () => {
   return (
     <section className="showcase">
+
+    <div id="carouselExampleIndicators" className="carousel slide">
+    <div className="carousel-indicators">
+      
+      {/* Buttons showing the active picture */}
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="0"
+        className="active"
+        aria-current="true"
+        aria-label="Slide 1"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="1"
+        aria-label="Slide 2"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="2"
+        aria-label="Slide 3"
+      ></button>
+    </div>
+    
+    {/* Displayed */}
+    <div className="carousel-inner">
+      <div className="carousel-item active">
         <div className="container">
             <div className="content">
             <div className="titles">
                     <p className="title-1">Welcome To Manero! 50% off</p>
                 </div> 
                 <div className="col-6">
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div className="carousel-item">
+        <div className="container">
+            <div className="content">
+            <div className="titles">
+                    <p className="title-1">Welcome To Manero! 50% extra</p>
+                </div> 
+                <div className="col-6">
 
                 </div>
             </div>
         </div>
-    </section>
-  )
-}
+      </div>
+
+      <div className="carousel-item">
+      <div className="container">
+            <div className="content">
+            <div className="titles">
+                    <p className="title-1">Welcome To Manero! </p>
+                </div> 
+                <div className="col-6">
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    
+    {/* Buttons for changing picture */}
+    <button
+      className="carousel-control-prev carouselbtn"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button
+      className="carousel-control-next carouselbtn"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+</section>
+);
+      }
 
 export default Showcase
