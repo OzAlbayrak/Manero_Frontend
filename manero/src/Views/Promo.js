81f1bbd62c058f9c5a6289015ba1d2ebc9fd78c5
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2023-05-08 10:02:08 +0200

    added promo

---
+++ manero/src/Views/Promo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PromoVoucher from '../components/individuals/PromoVoucher'
+
+const Promo = () => {
+    return (
+        <div><PromoVoucher /></div>
+    )
+}
+
+export default Promo
