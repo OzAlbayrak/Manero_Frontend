f28f4e5ceed033b55ac2734ab8f4a2a241b35874
Sam Jaudat <112482389+samjaudat@users.noreply.github.com>
2023-05-08 10:21:49 +0200

    added sccs for promo and voucher

---
+++ manero/src/components/sections/PromoVoucher.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SideIcon from '../individuals/SideIcon'
+
+const PromoVoucher = () => {
+    return (
+        <div>
+            <div className='top-back-and-text'>
+                <SideIcon />
+                <h1 className='promo-head'>My promocodes</h1>
+            </div>
+        </div>
+
+    )
+}
+
+export default PromoVoucher
