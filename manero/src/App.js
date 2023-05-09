17f796ec97694204da7ffe9c09fa032eaf18bc98
Per Norin <pelle.norin.79@gmail.com>
2023-05-09 11:16:40 +0200

    Added links in SignIn and SignUp & fixed  Header

--- manero/src/App.js
+++ manero/src/App.js
@@ -1,30 +1,31 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeView from "./Views/HomeView";
-import GetStartedHomeView from "./Views/GetStartedHomeView";
-import GetStartedWelcomeView from "./Views/GetStartedWelcomeView";
-import VerifyNumberView from "./Views/VerifyNumberView";
-import ConfirmCodeView from "./Views/ConfirmCodeView";
-import ResetedPassword from "./components/sections/ResetedPassword";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeView from './Views/HomeView';
+import GetStartedHomeView from './Views/GetStartedHomeView';
+import GetStartedWelcomeView from './Views/GetStartedWelcomeView';
+import VerifyNumberView from './Views/VerifyNumberView';
+import ConfirmCodeView from './Views/ConfirmCodeView';
+import ResetedPassword from './components/sections/ResetedPassword';
+import SignIn from './components/sections/SignIn';
+import SignUp from './components/sections/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/Reset" element={<ResetedPassword />} />
-          <Route path="/GetStarted" element={<GetStartedHomeView />} />
-          <Route
-            path="/GetStartedWelcome"
-            element={<GetStartedWelcomeView />}
-          />
-          <Route path="/VerifyNumber" element={<VerifyNumberView />} />
-          <Route path="/ConfirmCode" element={<ConfirmCodeView />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Routes>
+					<Route path='/' element={<HomeView />} />
+					<Route path='/Reset' element={<ResetedPassword />} />
+					<Route path='/GetStarted' element={<GetStartedHomeView />} />
+					<Route path='/GetStartedWelcome' element={<GetStartedWelcomeView />} />
+					<Route path='/VerifyNumber' element={<VerifyNumberView />} />
+					<Route path='/ConfirmCode' element={<ConfirmCodeView />} />
+					<Route path='/SignIn' element={<SignIn />} />
+					<Route path='/SignUp' element={<SignUp />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
