a90f2211f0cb09a83ad42b9f6781f82a4df464df
jesperforsen2022 <lester_orkester@hotmail.com>
2023-05-11 12:54:04 +0200

    added wishlistview + changed paths on links

--- manero/src/App.js
+++ manero/src/App.js
@@ -12,6 +12,7 @@ import AccountCreated from './components/sections/AccountCreated';
 import ForgotPassword from './components/sections/ForgotPassword';
 import ResetPassword from './components/sections/ResetPassword';
 import CartView from './Views/CartView';
+import WishlistView from './Views/WishlistView';
 
 function App() {
 	return (
@@ -30,6 +31,7 @@ function App() {
 					<Route path='/ForgotPassword' element={<ForgotPassword />} />
 					<Route path='/ResetPassword' element={<ResetPassword />} />
 					<Route path='/CartView' element={<CartView />} />
+					<Route path='/Wishlist' element={<WishlistView />} />
 				</Routes>
 			</Router>
 		</div>
---
+++ manero/src/Views/WishlistView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WishlistView = () => {
+  return (
+    <div>WishlistView</div>
+  )
+}
+
+export default WishlistView
