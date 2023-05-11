76c37459164c021c75b3520bb8d07f002bfc4955
Sam Jaudat <112482389+samjaudat@users.noreply.github.com>
2023-05-11 09:45:52 +0200

    css and promo pages done

--- manero/src/Views/HomeView.js
+++ manero/src/Views/HomeView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Showcase from '../components/sections/Showcase'
 import Showcase2 from '../components/sections/Showcase2'
 import ProductsGrid from '../components/sections/ProductsGrid'
-import PromoVoucher from '../components/sections/PromoVoucher'
+import PromoCodes from '../components/sections/Promocodes'
 import BestSellersGrid from '../components/sections/BestSellersGrid'
 
 
--- manero/src/components/sections/Promocodes.js
+++ manero/src/components/sections/Promocodes.js
@@ -1,15 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideIcon from "../individuals/SideIcon";
 
+
 const PromoCodes = () => {
+    const [currentLink, setCurrentLink] = useState(true);
+    const [usedLink, setUsedLink] = useState(false);
+    const [promoCode, setPromoCode] = useState("");
+
+    const handleCurrentClick = () => {
+        setCurrentLink(true);
+        setUsedLink(false);
+    };
+
+    const handleUsedClick = () => {
+        setCurrentLink(false);
+        setUsedLink(true);
+    };
+
+    const copyToClipboard = () => {
+        console.log("OK!");
+        navigator.clipboard.writeText(promoCode);
+        };
+
+    useEffect(() => {
+        const fetchData = async () => {
+        const response = await fetch("/");
+        const data = await response.text();
+        setPromoCode(data);
+        };
+        fetchData();
+    }, []);
+    
   return (
     <div>
       <div className="top-back-and-text">
         <SideIcon />
         <h1 className="promo-head">My promocodes</h1>
       </div>
+      <div className="voucher-link">
+            <a className={currentLink ? "promo-current-link active" : "promo-current-link"} href="/" onClick={handleCurrentClick}>Current</a>
+            <a className={usedLink ? "promo-used-link active" : "promo-used-link"} href="/" onClick={handleUsedClick}>Used</a>
+        </div>
+        <div>
+            <p style={promoCodeColor(20)}>20%<i class="fa-regular fa-copy fa-bounce" onClick={copyToClipboard}></i></p>
+            <p style={promoCodeColor(25)}>30%</p>
+            <p style={promoCodeColor(50)}>49%</p>
+        </div>
     </div>
   );
 };
+
+function promoCodeColor(percent) {
+    if (percent <= 15) {
+        return { color: "green" };
+      } else if (percent >= 50) {
+        return { color: "red" };
+      } else {
+        return { color: "orange" };
+      }
+}
+
 
 export default PromoCodes;
