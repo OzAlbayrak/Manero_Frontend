c0a2975a0e30be9fe591076b01aabca49f9fa4f2
FredrikJung <fredrik.jung@hotmail.com>
2023-05-12 12:33:26 +0200

    Fixed class to className in project

--- manero/src/components/sections/Promocodes.js
+++ manero/src/components/sections/Promocodes.js
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
 import SideIcon from "../individuals/SideIcon";
 
-
 const PromoCodes = () => {
-    const [currentLink, setCurrentLink] = useState(true);
-    const [usedLink, setUsedLink] = useState(false);
+  const [currentLink, setCurrentLink] = useState(true);
+  const [usedLink, setUsedLink] = useState(false);
 
-    const handleCurrentClick = () => {
-        setCurrentLink(true);
-        setUsedLink(false);
-    };
+  const handleCurrentClick = () => {
+    setCurrentLink(true);
+    setUsedLink(false);
+  };
 
-    const handleUsedClick = () => {
-        setCurrentLink(false);
-        setUsedLink(true);
-    };
+  const handleUsedClick = () => {
+    setCurrentLink(false);
+    setUsedLink(true);
+  };
 
-    const copyToClipboard = () => {
-        console.log("OK!");
-        navigator.clipboard.writeText(promoCode);
-    };
-    
+  const copyToClipboard = () => {
+    console.log("OK!");
+    navigator.clipboard.writeText(promoCode);
+  };
+
   return (
     <div>
       <div className="top-back-and-text">
@@ -28,26 +27,46 @@ const PromoCodes = () => {
         <h1 className="promo-head">My promocodes</h1>
       </div>
       <div className="voucher-link">
-            <a className={currentLink ? "promo-current-link active" : "promo-current-link"} href="/" onClick={handleCurrentClick}>Current</a>
-            <a className={usedLink ? "promo-used-link active" : "promo-used-link"} href="/" onClick={handleUsedClick}>Used</a>
-        </div>
-        <div>
-            <p style={promoCodeColor(20)}>20%<i class="fa-regular fa-copy fa-bounce" onClick={copyToClipboard}></i></p>
-            <p style={promoCodeColor(25)}>30%</p>
-            <p style={promoCodeColor(50)}>49%</p>
-        </div>
+        <a
+          className={
+            currentLink ? "promo-current-link active" : "promo-current-link"
+          }
+          href="/"
+          onClick={handleCurrentClick}
+        >
+          Current
+        </a>
+        <a
+          className={usedLink ? "promo-used-link active" : "promo-used-link"}
+          href="/"
+          onClick={handleUsedClick}
+        >
+          Used
+        </a>
+      </div>
+      <div>
+        <p style={promoCodeColor(20)}>
+          20%
+          <i
+            className="fa-regular fa-copy fa-bounce"
+            onClick={copyToClipboard}
+          ></i>
+        </p>
+        <p style={promoCodeColor(25)}>30%</p>
+        <p style={promoCodeColor(50)}>49%</p>
+      </div>
     </div>
   );
 };
 
 function promoCodeColor(percent) {
-    if (percent <= 15) {
-        return { color: "green" };
-      } else if (percent >= 50) {
-        return { color: "red" };
-      } else {
-        return { color: "orange" };
-      }
+  if (percent <= 15) {
+    return { color: "green" };
+  } else if (percent >= 50) {
+    return { color: "red" };
+  } else {
+    return { color: "orange" };
+  }
 }
 
 export default PromoCodes;
--- manero/src/components/sections/SignUp.js
+++ manero/src/components/sections/SignUp.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import Header from './Header';
-import InputField from '../individuals/InputField';
-import Button from '../individuals/Button';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Header from "./Header";
+import InputField from "../individuals/InputField";
+import Button from "../individuals/Button";
 
 const SignUp = () => {
-	const [formContent, setFormContent] = useState(null);
-	/*
+  const [formContent, setFormContent] = useState(null);
+  /*
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
@@ -18,66 +18,66 @@ const SignUp = () => {
 		console.log(res);
 	};
 */
-	return (
-		<div className='container d-flex flex-column'>
-			<Header
-				title={'Sign up'}
-				hasSideIcon={true}
-				isMenu={false}
-				hasCart={false}
-			></Header>
-			<div className='vr mx-auto'></div>
-			<p className='mx-auto my-3 headline'>Sign Up</p>
+  return (
+    <div className="container d-flex flex-column">
+      <Header
+        title={"Sign up"}
+        hasSideIcon={true}
+        isMenu={false}
+        hasCart={false}
+      ></Header>
+      <div className="vr mx-auto"></div>
+      <p className="mx-auto my-3 headline">Sign Up</p>
 
-			<form className='inputcontainer'>
-				{/*onSubmit={handleSubmit}*/}
-				<InputField
-					type={'text'}
-					name={'NAME'}
-					nameid={'name'}
-					placeholder={'Enter name'}
-				></InputField>
-				<InputField
-					type={'email'}
-					name={'EMAIL'}
-					nameid={'email'}
-					placeholder={'Enter email'}
-				></InputField>
-				<InputField
-					type={'password'}
-					name={'PASSWORD'}
-					nameid={'password'}
-					placeholder={'Enter password'}
-				></InputField>
-				<InputField
-					type={'password'}
-					name={'CONFIRM PASSWORD'}
-					nameid={'confirmPassword'}
-					placeholder={'Confirm password'}
-				></InputField>
-				<div className='text-center mx-auto px-3'>
-					<Button btnText={'SIGN UP'} btnType='submit' />
-				</div>
-			</form>
-			<p className='mx-auto text-light-color'>
-				Already have an account?{' '}
-				<NavLink to='/SignIn' className='text-dark-color register-link'>
-					Sign in.
-				</NavLink>
-			</p>
-			<div className='d-flex form-width justify-content-center mx-auto socials-btn-row'>
-				<button className='btn rounded-circle m-2'>
-					<i class='fa-brands fa-facebook-f'></i>
-				</button>
-				<button className='btn rounded-circle m-2'>
-					<i class='fa-brands fa-twitter'></i>
-				</button>
-				<button className='btn rounded-circle m-2'>
-					<i class='fa-brands fa-google-plus-g'></i>
-				</button>
-			</div>
-		</div>
-	);
+      <form className="inputcontainer">
+        {/*onSubmit={handleSubmit}*/}
+        <InputField
+          type={"text"}
+          name={"NAME"}
+          nameid={"name"}
+          placeholder={"Enter name"}
+        ></InputField>
+        <InputField
+          type={"email"}
+          name={"EMAIL"}
+          nameid={"email"}
+          placeholder={"Enter email"}
+        ></InputField>
+        <InputField
+          type={"password"}
+          name={"PASSWORD"}
+          nameid={"password"}
+          placeholder={"Enter password"}
+        ></InputField>
+        <InputField
+          type={"password"}
+          name={"CONFIRM PASSWORD"}
+          nameid={"confirmPassword"}
+          placeholder={"Confirm password"}
+        ></InputField>
+        <div className="text-center mx-auto px-3">
+          <Button btnText={"SIGN UP"} btnType="submit" />
+        </div>
+      </form>
+      <p className="mx-auto text-light-color">
+        Already have an account?{" "}
+        <NavLink to="/SignIn" className="text-dark-color register-link">
+          Sign in.
+        </NavLink>
+      </p>
+      <div className="d-flex form-width justify-content-center mx-auto socials-btn-row">
+        <button className="btn rounded-circle m-2">
+          <i className="fa-brands fa-facebook-f"></i>
+        </button>
+        <button className="btn rounded-circle m-2">
+          <i className="fa-brands fa-twitter"></i>
+        </button>
+        <button className="btn rounded-circle m-2">
+          <i className="fa-brands fa-google-plus-g"></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
