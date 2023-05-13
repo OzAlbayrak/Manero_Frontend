ed9e61a60f654b44d25761f8769ceb583a218fe5
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-13 16:19:53 +0200

    Added ShoppingCartProvider

--- manero/src/contexts/ShoppingCartContext.js
+++ manero/src/contexts/ShoppingCartContext.js
@@ -1,6 +1,55 @@
-import { createContext } from "react"
+import { createContext, useContext, useState } from "react"
 
 const ShoppingCartContext = createContext();
+export const useShoppingCartContext = () => {
+  return useContext(ShoppingCartContext);
+}
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalAmount: 0
+}
+
+export const ShoppingCartProvider = ({children}) => {
+
+  const [items, setItems] = useState(initialState.items)
+
+  const addItem = (product) => {
+    let _item = items.find(item => item.id === product.id)
+    if(_item)
+    {
+      setItems(items.map(item => 
+        item.id === _item.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+      ))
+    } 
+    else
+    {
+      setItems([...items, {id: product.id, name: product.name, quantity: 1}])
+    }
+  }
+
+  const removeItem = (product) => {
+    let _item = items.find(item => item.id === product.id)
+    if(_item.quantity > 1)
+    {
+      setItems(items.map(item => 
+        item.id === _item.id
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+      ))
+    } 
+    else
+    {
+      const itemList = items.filter(item => item.id !== _item.id)
+      setItems(itemList)
+    }
+  }
 
 
-
+  return <ShoppingCartContext.Provider value = { { items, addItem, removeItem } }>
+    {children}
+  </ShoppingCartContext.Provider>
+}
