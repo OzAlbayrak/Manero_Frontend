93076b82875b6302d08c1eb5f24a4f11afba6fcb
jesperforsen2022 <lester_orkester@hotmail.com>
2023-05-14 11:40:39 +0200

    changed some buttons

--- manero/src/Views/WishlistView.js
+++ manero/src/Views/WishlistView.js
@@ -2,20 +2,20 @@ import React from 'react';
 import Header from '../components/sections/Header';
 import Image from '../assets/images/wishlist_empty.svg';
 import MenuLinkIcons from '../components/individuals/MenuLinkIcons';
-import { useEffect } from 'react';
 import Wishlist from '../components/individuals/Wishlist';
 import { useWishlistContext } from '../contexts/WishlistContext';
+import { useShoppingCartContext } from '../contexts/ShoppingCartContext';
+import Rating from '../components/individuals/Rating'
 
 
 const WishListView = () => {
   const {favorites, removeFavorite} = useWishlistContext()
-
-
+  const {addItem} = useShoppingCartContext()
 
   return (
     <div className='container d-flex flex-column'>
         <Header
-            title={'MANERO'}
+            title={'Wishlist'}
             hasSideIcon={true}
             isMenu={true}
             hasCart={true}
@@ -37,13 +37,25 @@ const WishListView = () => {
               : 
               favorites.map(favorite => (
                 <div key={favorite.id}>
-                  <div>
-                    <img src={favorite.image} className="rounded float-left " alt="..." />
-                    {favorite.name}  -  ({favorite.price} kr)   x {favorite.quantity}
-                    <button onClick={() => removeFavorite(favorite)} className='btn btn-secondary'> Remove </button>
-
-                    
+                  <div className="wishlist-container">
+                    <div className="wishlist-img">
+                      <img src={favorite.image} alt="..." />
+                    </div>
+                    <div className="wishlist-info">
+                      <div>{favorite.name}</div>
+                      <div>${favorite.price}</div>
+                      <Rating/>
+                    </div>
+                    <div className="wishlist-btns">
+                    <button className="menu-link" onClick={() => removeFavorite(favorite)}>
+                      <i className="fa-regular fa-heart"></i>
+                    </button>
+                    <button className="menu-link" onClick={() => addItem(favorite)}>
+                      <i className="fa-light fa-bag-shopping"></i>
+                    </button>
+                    </div>                   
                   </div>
+                  <hr/>
                 </div>
                 
               ))
--- manero/src/components/individuals/Wishlist.js
+++ manero/src/components/individuals/Wishlist.js
@@ -12,7 +12,7 @@ const Wishlist = (prod) =>
 	return (
 		<div className='d-flex justify-content-center align-items-center' >
 			{prod.id ? addToWishlist(prod) : 				
-				<></>				
+				<div></div>				
 			}
 		</div>
 	);
