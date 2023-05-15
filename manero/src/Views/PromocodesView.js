8605b89a0976904085b13581e809c397edf0fbf5
Sam Jaudat <112482389+samjaudat@users.noreply.github.com>
2023-05-15 14:02:24 +0200

    added css and functions to promocodes

--- manero/src/Views/PromocodesView.js
+++ manero/src/Views/PromocodesView.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PromoVoucher from '../components/sections/PromoVoucher';
+import PromoCodes from '../components/sections/Promocodes';
 
 
 const PromocodesView = () => {
   return (
     <div>
         <PromoVoucher />
+        <PromoCodes />
     </div>
   )
 }
