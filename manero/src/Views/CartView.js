23683fd9c0831526d9196f4d46e01a8a3fd6b757
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-16 13:43:46 +0200

    Updated OrderView and CartView

--- manero/src/Views/CartView.js
+++ manero/src/Views/CartView.js
@@ -17,58 +17,74 @@ export const CartView = () => {
     //getProducts()
     localStorage.setItem('items', JSON.stringify(items));
     console.log(items)
-  }, [items])
+}, [items])
 
   return (
-    <div>
-    {
-      items.length !== 0 
-      ?
-        window.location.replace('/OrderView')
-      :
-      <div className='container d-flex flex-column'>
-        <Header title={"MANERO"} hasSideIcon={true} isMenu={true} hasCart={true}/>
-
-        <div className='text-center'>
-        <img className='mx-auto' src={Image}/>
-        <br></br>
-        <div className='vr'></div>
+    <div className='container d-flex flex-column'>
+      <Header title={"MANERO"} hasSideIcon={true} isMenu={true} hasCart={true}/>
         
-        <div className='text-center'>
-          <p className='mx-auto my-3 headline'>Your Cart Is Empty!</p>
-          <p className='mx-auto text-light-color'>Looks like you haven`t made your order yet</p>
-        </div> 
-        </div>
+ 
 
-        <div className='mx-auto'>
-        <NavLink to='/'>
-          <button className='btn rounded-pill my-3 custom-btn'>
-            SHOP NOW
-          </button>
-        </NavLink>
-        </div>
+          
+          <div>
+      
+            {
+              items.length === 0 
+              ?
+              <div className='text-center'>
+                <img className='mx-auto' src={Image}/>
+                <br></br>
+                <div className='vr'></div>
+                
+                <div className='text-center'>
+                  <p className='mx-auto my-3 headline'>Your Cart Is Empty!</p>
+                  <p className='mx-auto text-light-color'>Looks like you haven`t made your order yet</p>
+                </div> 
+                <div className='mx-auto'>
+                  <NavLink to='/'>
+                    <button className='btn rounded-pill my-3 custom-btn'>
+                      SHOP NOW
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
+              
+              : 
+              
+                <div className='text-center'>
+                  <img className='mx-auto' src={Image}/>
+                  <br></br>
+                {
+                  
+                  items.map(item => (
+                    <div key={item.id}>
+                      <div>
+                        <img src={item.image} className="rounded float-left " alt="..." />
+                        {item.name}  -  ({item.price} kr)   x {item.quantity}
+                        <button onClick={() => addItem(item)} className='btn btn-secondary'> + </button>
+                        <button onClick={() => removeItem(item)} className='btn btn-secondary'> - </button>
 
-        <MenuLinkIcons/>
+                      </div>
+                    </div>
+                    
+                  ))
+                }
+                <div className='mx-auto'>
+                  <NavLink to='/OrderView'>
+                    <button className='btn rounded-pill my-3 custom-btn'>
+                      Make Order
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
+            }  
+
+          </div>
+ 
+          
+          <MenuLinkIcons/>
         
       </div>
-      /*
-      items.map(item => (
-        <div key={item.id}>
-          <div>
-            <img src={item.image} className="rounded float-left " alt="..." />
-            {item.name}  -  ({item.price} kr)   x {item.quantity}
-            <button onClick={() => addItem(item)} className='btn btn-secondary'> + </button>
-            <button onClick={() => removeItem(item)} className='btn btn-secondary'> - </button>
-            <Rating />
-
-            
-          </div>
-        </div>
-        
-      ))
-      */
-    }
-    </div>
   )
 }
 
--- manero/src/Views/OrderView.js
+++ manero/src/Views/OrderView.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useShoppingCartContext } from '../contexts/ShoppingCartContext'
+import { useEffect } from 'react'
+import Rating from '../components/individuals/Rating'
 
 const OrderView = () => {
+
+  const {items, addItem, removeItem} = useShoppingCartContext()
+
+  useEffect(() => {
+    //getProducts()
+    localStorage.setItem('items', JSON.stringify(items));
+    console.log(items)
+  }, [items])
+
   return (
-    <div>OrderView</div>
+    
+    <div>
+        {
+    items.map(item => (
+        <div key={item.id}>
+          <div>
+            <img src={item.image} className="rounded float-left " alt="..." />
+            {item.name}  -  ({item.price} kr)   x {item.quantity}
+            <button onClick={() => addItem(item)} className='btn btn-secondary'> + </button>
+            <button onClick={() => removeItem(item)} className='btn btn-secondary'> - </button>
+            <Rating />
+
+            
+          </div>
+        </div>
+        
+      ))
+    }
+    </div>
   )
 }
 
