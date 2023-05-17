2ab5a1bcdf826dba6bc5b53870e511ee4136a0a2
jesperforsen2022 <lester_orkester@hotmail.com>
2023-05-17 11:44:19 +0200

    changed css on bottom menu

--- manero/src/components/individuals/MenuLinkIcons.js
+++ manero/src/components/individuals/MenuLinkIcons.js
@@ -2,24 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const MenuLinkIcons = () => {
+  //const [selectedPage, setSelectedPage] = useState();
+
   return (
-    <nav className="menuLinkIcons">
-      <NavLink to="/" className="menu-link">
-        <i className="fa-light fa-house"></i>
-      </NavLink>
-      <NavLink to="/Search" className="menu-link">
-        <i className="fa-light fa-magnifying-glass"></i>
-      </NavLink>
-      <NavLink to="/Cart" className="menu-link">
-        <i className="fa-light fa-bag-shopping"></i>
-      </NavLink>
-      <NavLink to="/Wishlist" className="menu-link">
-        <i className="fa-light fa-heart"></i>
-      </NavLink>
-      <NavLink to="/profile" className="menu-link">
-        <i className="fa-light fa-user"></i>
-      </NavLink>
-    </nav>
+    <div className="bottom-menu-container">
+      <nav className="menuLinkIcons">
+        <NavLink 
+        to="/" className="menu-link">
+          <i className="fa-light fa-house"></i>
+        </NavLink>
+        <NavLink to="/Search" className="menu-link">
+          <i className="fa-light fa-magnifying-glass"></i>
+        </NavLink>
+        <NavLink to="/Cart" className="menu-link">
+          <i className="fa-light fa-bag-shopping"></i>
+        </NavLink>
+        <NavLink to="/Wishlist" className="menu-link">
+          <i className="fa-light fa-heart"></i>
+        </NavLink>
+        <NavLink to="/profile" className="menu-link">
+          <i className="fa-light fa-user"></i>
+        </NavLink>
+      </nav>
+    </div>
   );
 };
 
