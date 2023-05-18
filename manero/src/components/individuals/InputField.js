bb8c0f41bf18bd10f9a9020109a6e761c8186c12
Per Norin <pelle.norin.79@gmail.com>
2023-05-18 17:05:16 +0200

    SignIn functionality done + created api.js

--- manero/src/components/individuals/InputField.js
+++ manero/src/components/individuals/InputField.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React from 'react';
 
 const InputField = ({
-  placeholder,
-  nameid,
-  name,
-  type,
-  value,
-  onChange,
-  style,
+	placeholder,
+	nameid,
+	name,
+	type,
+	value,
+	onChange,
+	style,
 }) => {
-  return (
-    <div className="text-center mx-auto">
-      <label className="baselabel" htmlFor={nameid}>
-        {name}
-      </label>
-      <input
-        className="baseinput"
-        type={type}
-        id={nameid}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        style={style}
-      />
-    </div>
-  );
+	return (
+		<div className='text-center mx-auto'>
+			<label className='baselabel' htmlFor={nameid}>
+				{name}
+			</label>
+			<input
+				className='baseinput'
+				type={type}
+				id={nameid}
+				value={value}
+				placeholder={placeholder}
+				onChange={onChange}
+				style={style}
+				name={name}
+			/>
+		</div>
+	);
 };
 
 export default InputField;
