c5cd15db3dbf2dc1da2dbfa34e56fe0b1967d4f7
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2023-05-25 21:12:36 +0200

    Css Uppdating

--- manero/src/Views/OrderFailedView.js
+++ manero/src/Views/OrderFailedView.js
@@ -11,17 +11,21 @@ const OrderFailed = () => {
             <h1>Sorry!your order has failed!</h1>
 
             <h4>Something went wrong. Please try again</h4>
-            <h4>to continue your order</h4>
+            <h4>to continue your order.</h4>
 
-            <div className='mx-auto'>
-                <NavLink to='/'>
-                    <button className='btn rounded-pill my-3 custom-btn'>
+            <div className=''>
+                <NavLink to='/Order'>
+                    <button className='btn rounded-pill my-3 custom-btn order-completed-b'>
                         TRY AGAIN
                     </button>
                 </NavLink>
             </div>
 
-            <div>GO TO MY PROFILE</div>
+            <div className='order-completed-p'>
+                <NavLink to='/'>
+                    <button className='order-completed-nav'>GO TO MY PROFILE</button>
+                </NavLink>
+            </div>
         </div>
     )
 }
