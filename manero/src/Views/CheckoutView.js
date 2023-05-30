713eecd10ddfb1fa1e7f3efb130c8668c8e538d0
Per Norin <pelle.norin.79@gmail.com>
2023-05-30 10:18:36 +0200

    Added clear address on signout + minor css fix

--- manero/src/Views/CheckoutView.js
+++ manero/src/Views/CheckoutView.js
@@ -1,90 +1,86 @@
-import React from 'react'
-import { useShoppingCartContext } from '../contexts/ShoppingCartContext'
-import { useEffect, useState } from 'react'
-import Header from '../components/sections/Header'
-import MenuLinkIcons from '../components/individuals/MenuLinkIcons'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { useShoppingCartContext } from '../contexts/ShoppingCartContext';
+import { useEffect, useState } from 'react';
+import Header from '../components/sections/Header';
+import MenuLinkIcons from '../components/individuals/MenuLinkIcons';
+import { NavLink } from 'react-router-dom';
 import SideIcon from '../components/individuals/SideIcon';
-import { PromoCodeProvider } from '../contexts/PromoCodeContext'
-import { useAddressContext } from "../contexts/AddressContext";
-import { useCreditCardContext } from "../contexts/CreditCardContext";
-import { useProfileContext } from "../contexts/ProfileContext";
+import { PromoCodeProvider } from '../contexts/PromoCodeContext';
+import { useAddressContext } from '../contexts/AddressContext';
+import { useCreditCardContext } from '../contexts/CreditCardContext';
+import { useProfileContext } from '../contexts/ProfileContext';
 
 const CheckoutView = () => {
-  const { items, addItem, removeItem } = useShoppingCartContext()
-  const [totAmountOfItems, setTotAmountOfItems] = useState(0);
-  const [totPrice, setTotPrice] = useState(0);
-  const [start, setStart] = useState(true);
-  const [comments, setComments] = useState('');
-  const [email, setEmail] = useState("order@myorder.com");
-  const [deliveryMethod, setdeliveryMethod] = useState("PostNord");
-  const [payment, setpayment] = useState("VISA CARD");
-  const [promoCodes, setPromoCodes] = useState([]);
-  const { profile } = useProfileContext();
-  const { addresses } = useAddressContext();
-  const { creditCards } = useCreditCardContext();
+	const { items, addItem, removeItem } = useShoppingCartContext();
+	const [totAmountOfItems, setTotAmountOfItems] = useState(0);
+	const [totPrice, setTotPrice] = useState(0);
+	const [start, setStart] = useState(true);
+	const [comments, setComments] = useState('');
+	const [email, setEmail] = useState('order@myorder.com');
+	const [deliveryMethod, setdeliveryMethod] = useState('PostNord');
+	const [payment, setpayment] = useState('VISA CARD');
+	const [promoCodes, setPromoCodes] = useState([]);
+	const { profile } = useProfileContext();
+	const { addresses } = useAddressContext();
+	const { creditCards } = useCreditCardContext();
 
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault()
+		const formData = {
+			price: totPrice,
+			profile: {
+				email: profile.email,
+				name: profile.name,
+				phoneNumber: '',
+			},
+			address: {
+				address: 'addresses[0].streetName',
+				postalCode: 'addresses[0].postalCode',
+				city: 'addresses[0].city',
+			},
+			products: items,
+			paymentMethod: payment,
+			comment: comments,
+			delivery: deliveryMethod,
+			PromoCodes: [],
+		};
 
-    const formData = {
-      price: totPrice,
-      profile: {
-        email: profile.email,
-        name: profile.name,
-        phoneNumber: ""
-      },
-      address: {
-        address: "addresses[0].streetName",
-        postalCode: "addresses[0].postalCode",
-        city: "addresses[0].city",
-      },
-      products: items,
-      paymentMethod: payment,
-      comment: comments,
-      delivery: deliveryMethod,
-      PromoCodes: []
-    }
+		console.log('profile:', formData.profile);
+		console.log('Products:', formData.products);
+		console.log('paymentMethod:', formData.paymentMethod);
+		console.log('Comment:', formData.comment);
+		console.log('Delivery:', formData.delivery);
+		console.log('PromoCode:', formData.promoCodes);
 
-    console.log('profile:', formData.profile);
-    console.log('Products:', formData.products);
-    console.log('paymentMethod:', formData.paymentMethod);
-    console.log('Comment:', formData.comment);
-    console.log('Delivery:', formData.delivery);
-    console.log('PromoCode:', formData.promoCodes);
+		fetch('https://sijb-cms22-backend.azurewebsites.net/api/Order', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(formData),
+		})
+			.then((res) => res.json())
+			.then((formData) => {
+				console.log(formData);
+				window.location.replace('/OrderCompleted');
+			})
+			.catch((error) => {
+				console.log(error);
+				window.location.replace('/OrderFailed');
+			});
+	};
 
+	const handleChange = (event) => {
+		setComments(event.target.value);
+	};
 
-    fetch('https://sijb-cms22-backend.azurewebsites.net/api/Order', {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Hantera inskickade kommentaren, t.ex. skicka till en API eller spara i en lokal lista
+		console.log('Skicka kommentar:', comments);
+		setComments('');
+	};
 
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData)
-    })
-      .then(res => res.json())
-      .then(formData => {
-        console.log(formData)
-        window.location.replace('/OrderCompleted')
-      })
-      .catch(error => {
-        console.log(error)
-        window.location.replace('/OrderFailed')
-      });
-  }
-
-  const handleChange = (event) => {
-    setComments(event.target.value);
-  };
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Hantera inskickade kommentaren, t.ex. skicka till en API eller spara i en lokal lista
-    console.log('Skicka kommentar:', comments);
-    setComments('');
-  };
-
-  /* const increaseTotItem = ((item) => {
+	/* const increaseTotItem = ((item) => {
      setTotAmountOfItems(totAmountOfItems + 1);
      setTotPrice(totPrice + item.price);
      addItem(item);
@@ -96,127 +92,115 @@ const CheckoutView = () => {
      removeItem(item);
    })*/
 
-  useEffect(() => {
-    localStorage.setItem('items', JSON.stringify(items));
-    console.log(items)
+	useEffect(() => {
+		localStorage.setItem('items', JSON.stringify(items));
+		console.log(items);
 
-    if (start === true) {
-      let pr = 0;
-      let itm = 0;
-      for (let i = 0; i < items.length; i++) {
-        itm += items[i].quantity;
-        pr += (items[i].price * items[i].quantity);
-      }
-      setTotAmountOfItems(itm)
-      setTotPrice(pr)
-      setStart(false)
-    }
-  }, [items])
+		if (start === true) {
+			let pr = 0;
+			let itm = 0;
+			for (let i = 0; i < items.length; i++) {
+				itm += items[i].quantity;
+				pr += items[i].price * items[i].quantity;
+			}
+			setTotAmountOfItems(itm);
+			setTotPrice(pr);
+			setStart(false);
+		}
+	}, [items]);
 
+	return (
+		<div className='d-flex flex-column vh-100'>
+			<div className='checkout-container m-1'>
+				<SideIcon />
+				<Header title={'Checkout'} isMenu={true}></Header>
+			</div>
 
+			<div className='checkout-info'>
+				<div className='checkout-info-order'>
+					<p className='semi'>My order</p>
+					<p className='totPrice'>
+						<div>${totPrice}</div>
+					</p>
+				</div>
 
-  return (
-    <div className='d-flex flex-column '>
-      <div className='checkout-container m-1'>
-        <SideIcon />
-        <Header
-          title={'Checkout'}
-          isMenu={true}
-        ></Header>
-      </div>
+				<div className='info-color'>
+					<hr />
+					<p className='totItem'>
+						{items.map((item) => (
+							<div key={item.id}>
+								<div className='check-inf'>
+									<div>{item.name}</div>
+									<div>
+										{item.quantity} x ${item.price}
+									</div>
+								</div>
+							</div>
+						))}
+					</p>
 
+					<p className='discount'>Discount</p>
+					<div></div>
 
-      <div className='checkout-info'>
-        <div className='checkout-info-order'>
-          <p className='semi'>My order</p>
-          <p className='totPrice'>
-            <div>
-              ${totPrice}
-            </div>
-          </p>
-        </div>
+					<div className='d-flex order-delivery-p'>
+						<p className='delivery'>Delivery</p>
+						<p className='odp'>Free</p>
+					</div>
 
-        <div className='info-color'>
-          <hr />
-          <p className='totItem'>
-            {
-              items.map(item => (
-                <div
-                  key={item.id}>
-                  <div className='check-inf'>
-                    <div >
-                      {item.name}
-                    </div>
-                    <div>
-                      {item.quantity} x ${item.price}
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
-          </p>
+					<hr />
+				</div>
+			</div>
 
-          <p className='discount'>Discount</p>
-          <div>
+			<div className='shipping-address'>
+				<p className='semi'>Shipping details</p>
+				<NavLink to='/Shippment'>
+					<i className='fa-regular fa-chevron-right'></i>
+				</NavLink>
+			</div>
+			<p className='p-info'>Nordkapsvägen 1, 136 57, Vega SVERIGE</p>
 
-          </div>
+			<hr />
+			<div className='shipping-address'>
+				<p className='semi'>Payment Method</p>
+				<NavLink to='/Payment'>
+					<i className='fa-regular fa-chevron-right'></i>
+				</NavLink>
+			</div>
+			<p className='p-info'>7741********6644</p>
 
+			<hr />
 
-          <div className='d-flex order-delivery-p' >
-            <p className='delivery'>Delivery</p>
-            <p className='odp'>Free</p>
-          </div>
+			<form className='checkout-form' onSubmit={handleOnSubmit}>
+				<label>COMMENT</label>
+				<div>
+					<textarea
+						className='area'
+						rows='8'
+						colum='10'
+						value={comments}
+						onChange={(e) => setComments(e.target.value)}
+						placeholder='Enter your comment'
+					/>
+				</div>
+				<div className='mx-auto button-container'>
+					<NavLink>
+						<button
+							className='btn rounded-pill my-3 custom-btn'
+							type='submit'
+							onClick={handleOnSubmit}
+						>
+							Confirm Order
+						</button>
+					</NavLink>
+				</div>
+			</form>
 
-          <hr />
-        </div>
-      </div>
-      
-      <div className='shipping-address'>
-        <p className='semi'>Shipping details</p>
-        <NavLink to='/Shippment'>
-          <i className='fa-regular fa-chevron-right'></i>
-        </NavLink>
-      </div>
-      <p className='p-info'>Nordkapsvägen 1, 136 57, Vega SVERIGE</p>
+			<MenuLinkIcons />
+		</div>
+	);
+};
 
-      <hr />
-      <div className='shipping-address'>
-        <p className='semi'>Payment Method</p>
-        <NavLink to='/Payment'>
-          <i className='fa-regular fa-chevron-right'></i>
-        </NavLink>
-      </div>
-      <p className='p-info'>7741********6644</p>
-
-      <hr />
-
-      
-
-      <form className='checkout-form' onSubmit={handleOnSubmit}>
-        <label>COMMENT</label>
-        <div>
-          <textarea className='area' rows="8" colum="10"
-            value={comments}
-            onChange={(e) => setComments(e.target.value)}
-            placeholder="Enter your comment"
-          />
-        </div>
-        <div className='mx-auto button-container'>
-          <NavLink>
-            <button className='btn rounded-pill my-3 custom-btn' type="submit" onClick={handleOnSubmit}>
-              Confirm Order
-            </button>
-          </NavLink>
-        </div>
-      </form>
-
-      <MenuLinkIcons className='menu-icons' />
-    </div>
-  )
-}
-
-export default CheckoutView
-
+export default CheckoutView;
 
 /* <p className='discount'>Discount
             <div>
--- manero/src/components/sections/SignIn.js
+++ manero/src/components/sections/SignIn.js
@@ -1,168 +1,165 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import Header from "./Header";
-import InputField from "../individuals/InputField";
-import Button from "../individuals/Button";
-import { logIn } from "../../utilities/api";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Header from './Header';
+import InputField from '../individuals/InputField';
+import Button from '../individuals/Button';
+import { logIn } from '../../utilities/api';
+import { useNavigate } from 'react-router-dom';
 import {
-  LoginSocialFacebook,
-  LoginSocialGoogle,
-  LoginSocialTwitter,
-} from "reactjs-social-login";
-import { useProfileContext } from "../../contexts/ProfileContext";
+	LoginSocialFacebook,
+	LoginSocialGoogle,
+	LoginSocialTwitter,
+} from 'reactjs-social-login';
+import { useProfileContext } from '../../contexts/ProfileContext';
 
 const SignIn = () => {
-  const navigate = useNavigate();
-  const [rememberMe, setRememberMe] = useState(false);
-  const [error, setError] = useState("");
-  const { handleResponse } = useProfileContext();
+	const navigate = useNavigate();
+	const [rememberMe, setRememberMe] = useState(false);
+	const [error, setError] = useState('');
+	const { handleResponse } = useProfileContext();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    setError("");
-    const formData = new FormData(e.target);
-    const res = Object.fromEntries(formData);
-    rememberMe ? (res.rememberMe = true) : (res.rememberMe = false);
-    console.log("res:", res);
-    console.log(JSON.stringify(res));
+		setError('');
+		const formData = new FormData(e.target);
+		const res = Object.fromEntries(formData);
+		rememberMe ? (res.rememberMe = true) : (res.rememberMe = false);
+		console.log('res:', res);
+		console.log(JSON.stringify(res));
 
-    const result = await logIn(res);
+		const result = await logIn(res);
 
-    switch (result.status) {
-      case 200:
-        {
-          const token = await result.text();
-          sessionStorage.setItem("apiAccessToken", token);
-          sessionStorage.setItem("provider", "local");
-          console.log(token);
-          navigate("/home");
-        }
-        break;
-      case 400:
-        {
-          const data = await result.json();
-          setError(data.title);
-        }
-        break;
-      case 401:
-        {
-          const data = await result.text();
-          setError(data);
-        }
-        break;
-      default:
-        setError("An unknown error occurred.");
-    }
-  };
+		switch (result.status) {
+			case 200:
+				{
+					const token = await result.text();
+					sessionStorage.setItem('apiAccessToken', token);
+					sessionStorage.setItem('provider', 'local');
+					console.log(token);
+					navigate('/home');
+				}
+				break;
+			case 400:
+				{
+					const data = await result.json();
+					setError(data.title);
+				}
+				break;
+			case 401:
+				{
+					const data = await result.text();
+					setError(data);
+				}
+				break;
+			default:
+				setError('An unknown error occurred.');
+		}
+	};
 
-  return (
-    <div className="container d-flex flex-column p-0">
-      <Header
-        title={"Sign in"}
-        hasSideIcon={true}
-        isMenu={false}
-        hasCart={false}
-      ></Header>
-      <div className="vr mx-auto"></div>
-      <p className="mx-auto my-3 headline">Welcome Back!</p>
-      <p className="mx-auto text-light-color">Sign in to continue</p>
-      <form className="inputcontainer" onSubmit={handleSubmit}>
-        <p className="text-danger text-center mx-auto">{error}</p>
-        <InputField
-          type={"email"}
-          name={"email"}
-          nameid={"email"}
-          placeholder={"Enter email"}
-          labelName={"email"}
-        ></InputField>
-        <InputField
-          type={"password"}
-          name={"password"}
-          nameid={"password"}
-          placeholder={"Enter password"}
-          labelName={"password"}
-        ></InputField>
-        <div className="d-flex justify-content-between container text-center my-3">
-          <div>
-            <input
-              type="checkbox"
-              id="rememberMe"
-              name="rememberMe"
-              value={rememberMe}
-              onChange={() => setRememberMe(!rememberMe)}
-            />
-            <label htmlFor="rememberMe" className="text-light-color">
-              Remember me
-            </label>
-          </div>
-          <NavLink
-            to="/ForgotPassword"
-            className="text-dark-color register-link"
-          >
-            Forgot password?
-          </NavLink>
-        </div>
-        <div className="text-center mx-auto px-3">
-          <Button btnText={"SIGN IN"} btnType="submit" />
-        </div>
-      </form>
-      <p className="mx-auto text-light-color">
-        Don't have an account?{" "}
-        <NavLink to="/SignUp" className="text-dark-color register-link">
-          Sign up.
-        </NavLink>
-      </p>
-      <div className="d-flex form-width justify-content-center mx-auto socials-btn-row">
-        <LoginSocialFacebook
-          appId="778333243702664"
-          onResolve={(res) => {
-            console.log("fb res: ", res);
-            handleResponse(res);
-            navigate("/");
-          }}
-          onReject={(error) => {
-            console.log("error:", error);
-          }}
-        >
-          <button className="btn rounded-circle m-2">
-            <i className="fa-brands fa-facebook-f"></i>
-          </button>
-        </LoginSocialFacebook>
-        <LoginSocialTwitter
-          client_id="TXJUeGJhRWYtaHhENFoySWNxR1I6MTpjaQ"
-          onResolve={(res) => {
-            handleResponse(res);
-            navigate("/");
-          }}
-          onReject={(error) => {
-            console.log(error);
-          }}
-        >
-          <button className="btn rounded-circle m-2">
-            <i className="fa-brands fa-twitter"></i>
-          </button>
-        </LoginSocialTwitter>
-        <LoginSocialGoogle
-          client_id="287952620391-q4761s4igqnl0uflbkv5jm3nqe594l3c.apps.googleusercontent.com"
-          scope="openid profile email"
-          onResolve={(res) => {
-            console.log("google resolve");
-            handleResponse(res);
-            navigate("/");
-          }}
-          onReject={(error) => {
-            console.log("google reject", error);
-          }}
-        >
-          <button className="btn rounded-circle m-2">
-            <i className="fa-brands fa-google-plus-g"></i>
-          </button>
-        </LoginSocialGoogle>
-      </div>
-    </div>
-  );
+	return (
+		<div className='container d-flex flex-column p-0'>
+			<Header
+				title={'Sign in'}
+				hasSideIcon={true}
+				isMenu={false}
+				hasCart={false}
+			></Header>
+			<div className='vr mx-auto'></div>
+			<p className='mx-auto my-3 headline'>Welcome Back!</p>
+			<p className='mx-auto text-light-color'>Sign in to continue</p>
+			<form className='inputcontainer' onSubmit={handleSubmit}>
+				<p className='text-danger text-center mx-auto'>{error}</p>
+				<InputField
+					type={'email'}
+					name={'email'}
+					nameid={'email'}
+					placeholder={'Enter email'}
+					labelName={'email'}
+				></InputField>
+				<InputField
+					type={'password'}
+					name={'password'}
+					nameid={'password'}
+					placeholder={'Enter password'}
+					labelName={'password'}
+				></InputField>
+				<div className='d-flex justify-content-between container text-center my-3'>
+					<div>
+						<input
+							type='checkbox'
+							id='rememberMe'
+							name='rememberMe'
+							value={rememberMe}
+							onChange={() => setRememberMe(!rememberMe)}
+						/>
+						<label htmlFor='rememberMe' className='text-light-color'>
+							Remember me
+						</label>
+					</div>
+					<NavLink to='/ForgotPassword' className='text-dark-color register-link'>
+						Forgot password?
+					</NavLink>
+				</div>
+				<div className='text-center mx-auto px-3'>
+					<Button btnText={'SIGN IN'} btnType='submit' />
+				</div>
+			</form>
+			<p className='mx-auto text-light-color'>
+				Don't have an account?{' '}
+				<NavLink to='/SignUp' className='text-dark-color register-link'>
+					Sign up.
+				</NavLink>
+			</p>
+			<div className='d-flex form-width justify-content-center mx-auto socials-btn-row'>
+				<LoginSocialFacebook
+					appId='778333243702664'
+					onResolve={(res) => {
+						console.log('fb res: ', res);
+						handleResponse(res);
+						navigate('/home');
+					}}
+					onReject={(error) => {
+						console.log('error:', error);
+					}}
+				>
+					<button className='btn rounded-circle m-2'>
+						<i className='fa-brands fa-facebook-f'></i>
+					</button>
+				</LoginSocialFacebook>
+				<LoginSocialTwitter
+					client_id='TXJUeGJhRWYtaHhENFoySWNxR1I6MTpjaQ'
+					onResolve={(res) => {
+						handleResponse(res);
+						navigate('/home');
+					}}
+					onReject={(error) => {
+						console.log(error);
+					}}
+				>
+					<button className='btn rounded-circle m-2'>
+						<i className='fa-brands fa-twitter'></i>
+					</button>
+				</LoginSocialTwitter>
+				<LoginSocialGoogle
+					client_id='287952620391-q4761s4igqnl0uflbkv5jm3nqe594l3c.apps.googleusercontent.com'
+					scope='openid profile email'
+					onResolve={(res) => {
+						console.log('google resolve');
+						handleResponse(res);
+						navigate('/home');
+					}}
+					onReject={(error) => {
+						console.log('google reject', error);
+					}}
+				>
+					<button className='btn rounded-circle m-2'>
+						<i className='fa-brands fa-google-plus-g'></i>
+					</button>
+				</LoginSocialGoogle>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
