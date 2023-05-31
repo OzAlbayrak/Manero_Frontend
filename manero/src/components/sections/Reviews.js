820c46e873f6daa982ffe4ea5de86ae86adec74e
Sam Jaudat <112482389+samjaudat@users.noreply.github.com>
2023-05-31 15:47:38 +0200

    added reviews css

--- manero/src/components/sections/Reviews.js
+++ manero/src/components/sections/Reviews.js
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
 import Header from "../sections/Header";
 import Image from "../../assets/images/LeaveReview.svg";
-import StarRating from "../individuals/RatingStars";
-import Button from "../individuals/Button";
 
 const Reviews = () => {
-  const [reviews, setReviews] = useState([]);
-  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
 
-  const handleReviewSubmit = (event) => {
-    event.preventDefault();
+  const handleReviewSubmit = () => {
+    console.log("Review Text:", reviewText);
+      
+    setRating(0);
+    setReviewText("");
 
-    const rating = event.target.rating.value;
+  };
 
-    if (rating > 0) {
-      const newReview = {
-        comment,
-        rating,
-      };
+  const handleReviewTextChange = (e) => {
+    setReviewText(e.target.value);
+  };
 
-      setReviews([...reviews, newReview]);
-      setComment("");
-    }
+  const ReviewInput = ({ value, onChange, placeholder, name }) => {
+    return (
+      <textarea className="review-baseinput"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        name={name}
+      ></textarea>
+    );
+  };
+
+  const ReviewButton = ({ btnText, handleClick }) => {
+    return <button onClick={handleClick}>{btnText}</button>;
   };
 
   return (
@@ -33,44 +43,47 @@ const Reviews = () => {
         hasCart={false}
       />
       <div className="revpicture">
-        <img src={Image} alt="LeaveReview"></img>
+        <img src={Image} alt="LeaveReview" />
       </div>
       <div className="vr mx-auto"></div>
       <div className="revText">
         <p>Please Rate the Quality of our service for the order!</p>
       </div>
-      <div className="star-rating">
-        <StarRating name="rating" />
+      <div className="star-rating-review">
+        {[...Array(5)].map((star, index) => {
+          index += 1;
+          return (
+            <span
+              type="button"
+              key={index}
+              className={index <= (hover || rating) ? "on" : "off"}
+              onClick={() => setRating(index)}
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(rating)}
+            >
+              <span className="star">&#9733;</span>
+            </span>
+          );
+        })}
       </div>
       <p className="revText-small">
-        Your comments and suggestions help us improve the service quality
-        better!
+        Your comments and suggestions help us improve the service quality better!
       </p>
-      <form onSubmit={handleReviewSubmit}>
-        <div className="text-center-review-input">
-          <label className="baselabel-review-input" />
-          <input className="review-input-base" />
+      <div className="review-input-container">
+      <div className="review-text-center-label">
+    <label className="review-label">Review</label>
+        <ReviewInput
+          value={reviewText}
+          onChange={handleReviewTextChange}
+          placeholder="Enter your review"
+          name="review"
+        />
+      </div>
+      </div>
+      <div className="review-btn d-flex aligns-items-center justify-content-center">
+        <div className="submit-button-wrapper mt-4">
+          <ReviewButton btnText="Submit" handleClick={handleReviewSubmit} />
         </div>
-        <div className="review-btn d-flex aligns-items-center justify-content-center">
-            <div className="submit-button-wrapper mt-4">
-              <Button
-                btnText={"SUBMIT"}
-                type={"submit"}
-                onClick={handleReviewSubmit}
-              />
-          </div>
-        </div>
-      </form>
-      <div className="review-list">
-        <h3>Reviews:</h3>
-        <ul>
-          {reviews.map((review, index) => (
-            <li key={index}>
-              <p>Rating: {review.rating}</p>
-              <p>Comment: {review.comment}</p>
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   );
